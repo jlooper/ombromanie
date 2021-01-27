790a72579c4424979a9628b0704503b2057647b6
Jen Looper <jen.looper@gmail.com>
2021-01-27 10:22:36 -0500

    edit for key

--- api/getKey/index.js
+++ api/getKey/index.js
@@ -1,6 +1,5 @@
 module.exports = function (context) {
-	let key = process.env['VUE_APP_MAP_KEY'];
+	let key = process.env['VUE_APP_SPEECH_KEY'];
 	context.res = { body: key };
 	context.done();
-	
 };
