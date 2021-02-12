1744b7abb57981132643c60ded44972df2c070b3
Jen Looper <jen.looper@gmail.com>
2021-02-11 22:27:52 -0500

    removing audio recorder

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import AudioRecorder from 'vue-audio-recorder';
 
-Vue.use(AudioRecorder);
 Vue.config.productionTip = false;
 
 new Vue({
